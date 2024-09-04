1dc916b713352afdc7f9248324b255e2164e5d9b
Vishnu Narugondla <109599759+vishnu6960@users.noreply.github.com>
2024-09-04 12:15:33 +0530

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,26 +1,25 @@
 var br = document.createElement("br")
 function convert1(){
-     var fromcurrency = document.getElementById("from").value
-     var tocurrency = document.getElementById("to").value
-     var oldamount = document.getElementById("amount").value
-    //  console.log(typeof oldamount)
-	  oldamount = (Number(oldamount)).toFixed(4)
-    // console.log(oldamount)
+    var fromcurrency = document.getElementById("from").value
+    var tocurrency = document.getElementById("to").value
+    var oldamount = document.getElementById("amount").value
+      //  console.log(typeof oldamount)
+    oldamount = (Number(oldamount)).toFixed(4)
+      // console.log(oldamount)
 
-     //var url = 'https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency?have='+fromcurrency+'&want='+tocurrency+'&amount='+oldamount;
-     var url =  'https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from='+fromcurrency+'&to='+tocurrency+'&amount='+oldamount
-	 const options = {
-    method: 'GET',
-    headers: {
-		'X-RapidAPI-Key': 'f5adf660e9msh1aec9d97f216132p18a5e1jsnee41e6dd8e8f',
-		'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
-    }
-  }; 
+    var url =  'https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from='+fromcurrency+'&to='+tocurrency+'&amount='+oldamount
+    const options = {
+      method: 'GET',
+      headers: {
+      'X-RapidAPI-Key': 'f5adf660e9msh1aec9d97f216132p18a5e1jsnee41e6dd8e8f',
+      'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
+      }
+    }; 
 
-  fetch(url,options)
-  .then(response => response.json())
-  //.then(response => console.log(Object.values(response)[2].rate))
-  //.then(response => console.log(Object.values(response)))
-  .then(response=>document.getElementById("output").innerHTML=((oldamount*(Object.values(response)[2].rate)).toFixed(4)+' '+tocurrency))
-  .then(response => document.getElementById("input").innerHTML= oldamount+' '+fromcurrency+' '+'=')
-  }
+    fetch(url,options)
+    .then(response => response.json())
+    //.then(response => console.log(Object.values(response)[2].rate))
+    //.then(response => console.log(Object.values(response)))
+    .then(response=>document.getElementById("output").innerHTML=((oldamount*(Object.values(response)[2].rate)).toFixed(4)+' '+tocurrency))
+    .then(response => document.getElementById("input").innerHTML= oldamount+' '+fromcurrency+' '+'=')
+}
