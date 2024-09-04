b48ddf995b9c00bac6ce6de5459c2bc9f197ee84
vishnu <kannasri113@gmail.com>
2024-09-04 12:01:27 +0530

    updating the decimal values

--- currency.js
+++ currency.js
@@ -3,6 +3,7 @@ function convert1(){
      var fromcurrency = document.getElementById("from").value
      var tocurrency = document.getElementById("to").value
      var oldamount = document.getElementById("amount").value
+	  oldamount = Number(oldamount).toFixed(4)
 
 	 //'https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from='+fromcurrency+'&to='+tocurrency+'&amount='+oldamount
 
@@ -11,7 +12,7 @@ function convert1(){
 	 const options = {
     method: 'GET',
     headers: {
-		'X-RapidAPI-Key': 'ad0c9c7bd0msh01ec56be200f01dp14e47ajsn69c552a045a5',
+		'X-RapidAPI-Key': 'f5adf660e9msh1aec9d97f216132p18a5e1jsnee41e6dd8e8f',
 		'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
     }
   }; 
@@ -20,6 +21,6 @@ function convert1(){
   .then(response => response.json())
   //.then(response => console.log(Object.values(response)[2].rate))
   //.then(response => console.log(Object.values(response)))
-  .then(response=>document.getElementById("output").innerHTML=(oldamount*(Object.values(response)[2].rate)+' '+tocurrency))
+  .then(response=>document.getElementById("output").innerHTML=((oldamount*(Object.values(response)[2].rate)).toFixed(4)+' '+tocurrency))
   .then(response => document.getElementById("input").innerHTML= oldamount+' '+fromcurrency+' '+'=')
   }
